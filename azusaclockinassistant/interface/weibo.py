cc341e8992e85799c8ee1ee2913e09b7d9e8b56c
Bruce Zhang <zttt183525594@gmail.com>
2022-08-27 14:51:23 +0800

    fix

--- azusaclockinassistant/interface/weibo.py
+++ azusaclockinassistant/interface/weibo.py
@@ -11,12 +11,14 @@ class Weibo(BaseInterface):
     """新浪微博"""
     def __init__(self):
         self.cookie = os.environ.get('WEIBO_COOKIE', '')
-        self.super_indexes = os.environ.get('WEIBO_SUPER_INDEX', '').split(',')
         self.headers.update({
             'Cookie': self.cookie,
             'Referer': 'https://weibo.com',
             'client-version': 'v2.34.91',
             'server-version': 'v2022.08.26.2',
+            'x-requested-with': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/104.0.0.0 Safari/537.36',
         })
         super(Weibo, self).__init__()
 
@@ -25,15 +27,22 @@ class Weibo(BaseInterface):
         """微博超话签到"""
         if self.cookie == '':
             return False, '未配置 Cookie'
-        for id_ in self.super_indexes:
-            if id_ == '':
-                continue
-            ok, msg = await self._super_index_clockin(id_)
+        super_indexes = await self._get_fav_super_index()
+        for id_, name in super_indexes:
+            _, id__ = id_.split(':')
+            ok, msg = await self._super_index_clockin(id__)
             if not ok:
-                click.echo(f'超话 {id_} 签到失败：{msg}')
+                click.echo(f'超话 {name} 签到失败：{msg}')
             else:
-                click.echo(f'超话 {id_} 签到成功')
+                click.echo(f'超话 {name} 签到成功')
         return True, '执行完成'
+
+    async def _get_fav_super_index(self):
+        async with self.client.get('https://weibo.com/ajax/profile/topicContent', params={
+            'tabid': '231093_-_chaohua',
+        }) as resp:
+            data = await resp.json()
+            return [(i.get('oid'), i.get('title')) for i in data.get('data', {}).get('list', [])]
 
     async def _super_index_clockin(self, id_):
         async with self.client.get('https://weibo.com/p/aj/general/button', params={
@@ -50,9 +59,6 @@ class Weibo(BaseInterface):
             'ua': self.BROWSER_UA,
             'screen': '1920*1080',
             '__rnd': str(int(round(time.time() * 1000))),
-        }, headers={
-            'Referer': f'https://weibo.com/p/{id_}/super_index',
-            'x-requested-with': 'XMLHttpRequest',
         }) as resp:
             if 'json' not in resp.content_type:
                 return False, '响应格式非JSON'
